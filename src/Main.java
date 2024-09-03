17d23dbb557c3b99d376011f1f20df1db552a433
pugeaale <pugeaale@gmail.com>
2024-09-03 18:19:36 +0200

    :sparkles: implementing reversal cypher

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
@@ -21,13 +22,91 @@ public class Main {
     }
 
     private static void decodeFile() {
-        System.out.print("Enter the path of the file to decode : ");
-        scanner.nextLine();
+        doDecoding(getFilePath());
     }
 
     private static void encodeFile() {
-        System.out.print("Enter the path of the file to encode : ");
-        scanner.nextLine();
+        doEncoding(getFilePath());
+    }
+
+    private static void doEncoding(String path) {
+        File f = new File(path);
+        try {
+            System.out.println(f.getName());
+            Scanner s = new Scanner(f);
+            String encodedPath = "encoded_" + f.getName();
+            FileWriter fw = null;
+            try {
+                fw = new FileWriter(encodedPath, true);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            while(s.hasNextLine()) {
+                System.out.println("Encoding the file ...");
+                String line = encrypt(s.nextLine());
+                try {
+                    fw.write(line+"\n");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            scanner.close();
+            try {
+                fw.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String encrypt(String s) {
+        StringBuilder ciphertext = new StringBuilder(s);
+        return ciphertext.reverse().toString();
+    }
+
+    private static String decrypt(String s) {
+        StringBuilder plaintext = new StringBuilder(s);
+        return plaintext.reverse().toString();
+    }
+
+    private static void doDecoding(String path) {
+        File f = new File(path);
+        try {
+            System.out.println(f.getName());
+            Scanner s = new Scanner(f);
+            String encodedPath = "decoded_" + f.getName();
+            FileWriter fw = null;
+            try {
+                fw = new FileWriter(encodedPath, true);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            while(s.hasNextLine()) {
+                System.out.println("Decoding the file ...");
+                String line = decrypt(s.nextLine());
+                try {
+                    fw.write(line+"\n");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            try {
+                fw.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static String getFilePath() {
+        System.out.print("Enter the path of the file : ");
+        return scanner.nextLine();
     }
 
     private static void displayMenu() {
